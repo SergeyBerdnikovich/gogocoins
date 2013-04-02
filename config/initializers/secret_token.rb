868f69c1249debc7422a01f5dec2b86e14e64a03
SergeyBerdnikovich <atom@tut.by>
2013-04-02 12:09:52 +0300

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gogocoins::Application.config.secret_token = '4dd4994937bac2427fa07d69f0a159304652d4e99b6daab280d4c8ad7c5e7ad764c9950229b8945896f7195e9291889443854ee2568e31db799196cfe5458c1d'
