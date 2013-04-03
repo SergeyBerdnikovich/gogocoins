bbf3be8119b70c33d43708c1b1618b02269e2e52
SergeyBerdnikovich <atom@tut.by>
2013-04-03 13:40:01 +0300

    issues/61 - added breadcrumb to categories index and products show

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,27 @@
 module CategoriesHelper
+  def categories_breadcrumb(parent_id)
+    parent = parent_id
+    links = []
+    while parent != 0 do
+      category = Category.find(parent)
+      parent = category.parent
+      if category.id != parent_id.to_i
+        links << link_to(category.name, categories_path(:parent_id => category.id))
+      else
+        links << category.name
+      end
+    end
+    links.reverse
+  end
+  def product_breadcrumb(product)
+    parent = @product.category.id
+    links = []
+    while parent != 0 do
+      category = Category.find(parent)
+      parent = category.parent
+
+      links << link_to(category.name, categories_path(:parent_id => category.id))
+    end
+    links.reverse << product.title
+  end
 end
